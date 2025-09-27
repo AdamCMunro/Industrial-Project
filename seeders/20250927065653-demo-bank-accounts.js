f24249a9a1f280b204f9f43fb94e421fc06e26ea
matthewmcccc <xyzmonte@protonmail.com>
2025-09-27 09:45:34 +0100

    fix again..

--- seeders/20250927065653-demo-bank-accounts.js
+++ seeders/20250927065653-demo-bank-accounts.js
@@ -7,7 +7,7 @@ module.exports = {
     const bankAccounts = Array.from({ length: 10 }).map(() => ({
       accountnumber: faker.finance.accountNumber(),
       sortnumber: faker.finance.routingNumber(),
-      cvv: faker.number.int({ min: 100, max: 999 }), // should we be storing cvvs? scary scary stuff
+      cvv: faker.number.int({ min: 100, max: 999 }), // should we be storing cvvs? 🤔
       balance: faker.number.int({ min: 0, max: 100000 })
     }));
 
